0dba2d927885fe9ce6fbbcc3edb6e322b31212f3
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-29 14:47:43 +0545

    Create native stack navigator

---
+++ src/components/screens/RootNativeStackScreen.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+const RootNativeStackScreen = () => {
+  
+  <NavigationContainer>
+    <Stack.Navigator>
+      
+    </Stack.Navigator>
+
+  </NavigationContainer>
+  
+}
+export default RootNativeStackScreen;
