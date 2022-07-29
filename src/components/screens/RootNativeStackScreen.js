cc0a69609d9b97a779db16955686c90c080c0eda
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-29 16:14:07 +0545

    Added useMemo hook

--- src/components/screens/RootNativeStackScreen.js
+++ src/components/screens/RootNativeStackScreen.js
@@ -1,15 +1,32 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {View, ActivityIndicator} from 'react-native';
 import LoginScreen, {styles} from './LoginScreen';
 import SplashScreen from './SplashScreen';
 import RegisterScreen from './RegisterScreen';
+import HomeScreen from './HomeScreen';
+import {AuthContext} from '../context/context';
 
 const Stack = createNativeStackNavigator();
 const RootNativeStackScreen = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [userToken, setUserToken] = useState(null);
+
+  const authContext = useMemo(() => ({
+    signIn: () => {
+      setUserToken('Acagsa1215@3$ahvaa');
+      setIsLoading(false);
+    },
+    signUp: () => {
+      setUserToken('Acagsa1215@3$ahvaa');
+      setIsLoading(false);
+    },
+    signOut: () => {
+      setUserToken(null);
+      setIsLoading(false);
+    },
+  }));
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,27 +35,37 @@ const RootNativeStackScreen = () => {
   }, []);
   if (isLoading) {
     return (
-      <view style={styles.indicator}>
+      <View style={styles.indicator}>
         <ActivityIndicator size="large" />
-      </view>
+      </View>
     );
   }
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Group screenOptions={{headerShown: false}}>
-          <Stack.Screen
-            name="SplashScreen"
-            component={SplashScreen}></Stack.Screen>
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}></Stack.Screen>
-          <Stack.Screen
-            name="RegisterScreen"
-            component={RegisterScreen}></Stack.Screen>
-        </Stack.Group>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {userToken === null ? (
+            <Stack.Group screenOptions={{headerShown: false}}>
+              <Stack.Screen
+                name="SplashScreen"
+                component={SplashScreen}></Stack.Screen>
+              <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}></Stack.Screen>
+              <Stack.Screen
+                name="RegisterScreen"
+                component={RegisterScreen}></Stack.Screen>
+            </Stack.Group>
+          ) : (
+            <Stack.Group>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}></Stack.Screen>
+            </Stack.Group>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 };
 export default RootNativeStackScreen;
