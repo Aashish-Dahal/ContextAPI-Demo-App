4602b5f0d22fe206950bc709a1b53d56b3403d82
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-29 15:39:27 +0545

    feat: Register Screen

--- src/components/screens/RegisterScreen.js
+++ src/components/screens/RegisterScreen.js
@@ -9,7 +9,30 @@ const RegisterScreen = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Register</Text>
+        <Text style={styles.text}>Create your account</Text>
+      </View>
+      <View style={styles.footer}>
+        <InputField icon="email-outline" hintText="Email address" />
+        <InputField icon="lock-outline" hintText="Email address" />
+        <InputField icon="lock-outline" hintText="Email address" />
+        <View style={styles.textPrivate}>
+          <Text style={styles.color_textPrivate}>
+            By signing up you agree to our
+          </Text>
+          <Text style={[styles.color_textPrivate, {fontWeight: 'bold'}]}>
+            Terms of service
+          </Text>
+          <Text style={styles.color_textPrivate}> and</Text>
+          <Text style={[styles.color_textPrivate, {fontWeight: 'bold'}]}>
+            Privacy policy
+          </Text>
+        </View>
+
+        <GradientButton style={styles.signIn} text="Sign Up" />
+        <Button text="Sign In" />
+      </View>
     </View>
   );
 };
