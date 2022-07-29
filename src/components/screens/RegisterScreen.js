2c8c6f6d5900bfe7c0c0fc317fabea241cc333a3
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-29 15:38:39 +0545

    Added Register screen Navigation

---
+++ src/components/screens/RegisterScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import Button from '../Button';
+import GradientButton from '../GradientButton';
+import InputField from '../InputField';
+import {styles} from './LoginScreen';
+import {useNavigation} from '@react-navigation/native';
+const RegisterScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+export default RegisterScreen;
