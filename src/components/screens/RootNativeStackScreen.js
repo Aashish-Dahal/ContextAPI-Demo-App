63ee645765ee15a0b2122737e7641fe6cf41b440
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-29 15:47:00 +0545

    Added Register Screen in stack

--- src/components/screens/RootNativeStackScreen.js
+++ src/components/screens/RootNativeStackScreen.js
@@ -1,16 +1,30 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, ActivityIndicator} from 'react-native';
+import LoginScreen, {styles} from './LoginScreen';
+import SplashScreen from './SplashScreen';
+import RegisterScreen from './RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 const RootNativeStackScreen = () => {
-  
-  <NavigationContainer>
-    <Stack.Navigator>
-      
-    </Stack.Navigator>
-
-  </NavigationContainer>
-  
-}
+ 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}></Stack.Screen>
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}></Stack.Screen>
+          <Stack.Screen
+            name="RegisterScreen"
+            component={RegisterScreen}></Stack.Screen>
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 export default RootNativeStackScreen;
